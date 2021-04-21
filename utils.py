c73a52d3c3f49718279f4801d4a136d6760613cb
Ilya Plotnikov <ilyaplotnikoff@gmail.com>
2021-04-21 11:25:41 +0300

    01-review_fixes

--- utils.py
+++ utils.py
@@ -2,21 +2,19 @@ import random
 import string
 import os
 
+from config import FILENAME_LENGTH
 
-def generate_filename(length=8):
+
+def generate_filename(length=FILENAME_LENGTH):
     return "{}.txt".format(generate_string(length))
 
 
-def generate_string(length=8):
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for _ in range(length))
+def generate_string(length):
+    return ''.join(
+        random.choice(
+            string.ascii_lowercase +
+            string.digits) for _ in range(length))
 
 
-def get_or_create_storage(storage_folder):
-    path = os.getcwd()
-    path = os.path.join(path, storage_folder)
-
-    if not os.path.exists(path):
-        os.makedirs(path)
-
-    return path
+def get_filepath(path, filename):
+    return os.path.join(path, filename)
